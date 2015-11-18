445984d94a01ab509f48335b9cb35da2cd57f47f
Kevin Schroeder <kschroeder@mirageworks.com>
2015-11-18 07:16:12 -0600

    Split up Magium Magento and Gmail components into separate libraries.

--- lib/Magento/Actions/Checkout/PaymentMethods/PaymentMethodInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Magium\Magento\Actions\Checkout\PaymentMethods;
-
-interface PaymentMethodInterface
-{
-    public function pay($requirePayment);
-}
--- lib/Magento/Actions/Checkout/Steps/StepInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Magium\Magento\Actions\Checkout\Steps;
-
-interface StepInterface
-{
-    public function execute();
-}
