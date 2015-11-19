427d4e281642f68c4d5f33fc88bb9fa72f36b573
Kevin Schroeder <kschroeder@mirageworks.com>
2015-11-19 17:10:54 -0600

    Created a convenience constructor for the abstract extractor

--- lib/Extractors/AbstractExtractor.php
+++ lib/Extractors/AbstractExtractor.php
@@ -2,10 +2,33 @@
 
 namespace Magium\Extractors;
 
+use Magium\AbstractTestCase;
+use Magium\WebDriver\WebDriver;
+
 abstract class AbstractExtractor
 {
 
     protected $values = [];
+    protected $webDriver;
+    protected $testCase;
+
+    /**
+     * AbstractExtractor constructor.
+     *
+     * Override this if you have other dependencies.  This is done as a convenience
+     *
+     * @param WebDriver $webDriver
+     * @param AbstractTestCase $testCase
+     */
+
+    public function __construct(
+        WebDriver           $webDriver,
+        AbstractTestCase    $testCase
+    )
+    {
+        $this->webDriver        = $webDriver;
+        $this->testCase         = $testCase;
+    }
 
     public function getValue($id)
     {
