c900f7b118066b070a75815c8fd3dbc55bf14433
Kevin Schroeder <kschroeder@mirageworks.com>
2015-11-23 17:12:37 -0600

    Added some global functionality for handling themes.

--- lib/Extractors/AbstractExtractor.php
+++ lib/Extractors/AbstractExtractor.php
@@ -3,6 +3,7 @@
 namespace Magium\Extractors;
 
 use Magium\AbstractTestCase;
+use Magium\Themes\ThemeConfigurationInterface;
 use Magium\WebDriver\WebDriver;
 
 abstract class AbstractExtractor
@@ -11,23 +12,29 @@ abstract class AbstractExtractor
     protected $values = [];
     protected $webDriver;
     protected $testCase;
+    protected $theme;
 
     /**
      * AbstractExtractor constructor.
      *
-     * Override this if you have other dependencies.  This is done as a convenience
+     * Override this if you have other dependencies.  This is done as a convenience.
+     *
+     * This may change as I'm not quite sure of the best way to handle theme classes at the moment.
      *
      * @param WebDriver $webDriver
      * @param AbstractTestCase $testCase
+     * @param ThemeConfigurationInterface $theme
      */
 
     public function __construct(
         WebDriver           $webDriver,
-        AbstractTestCase    $testCase
+        AbstractTestCase    $testCase,
+        ThemeConfigurationInterface $theme
     )
     {
         $this->webDriver        = $webDriver;
         $this->testCase         = $testCase;
+        $this->theme            = $theme;
     }
 
     public function getValue($id)
