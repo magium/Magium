c077cbac7a12f668341b95edd5ebbc988dde6fd1
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-04 16:18:43 -0600

    Two things 1) added support for multiple base namespaces 2) A base theme interface which will have basic declarations, such as a home page link

---
+++ lib/Themes/BaseThemeInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Magium\Themes;
+
+interface BaseThemeInterface extends ThemeConfigurationInterface
+{
+    public function getHomeXpath();
+
+}
--- lib/Themes/ThemeConfigurationInterface.php
+++ lib/Themes/ThemeConfigurationInterface.php
@@ -12,4 +12,6 @@ namespace Magium\Themes;
 interface ThemeConfigurationInterface
 {
 
+
+
 }
