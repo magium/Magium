afc180188cca4e4ff6a82a2c84a701373accd247
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-04 16:35:15 -0600

    Fixed class resolution

---
+++ tests/AbstractTestCase/ClassResolutionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Magium\AbstractTestCase {
+
+    use Magium\AbstractTestCase;
+    use Magium\Navigators\Home;
+
+    class ClassResolutionTest extends AbstractTestCase
+    {
+        public function testGetHomeWhichIsActuallyTestingClassResolution()
+        {
+
+            $this->switchThemeConfiguration('Magium\BaseTheme');
+            self::assertInstanceOf('Magium\Navigators\Home', $this->getNavigator(Home::NAVIGATOR));
+        }
+    }
+}
+
+namespace Magium {
+
+    use Magium\Themes\BaseThemeInterface;
+
+    class BaseTheme implements BaseThemeInterface {
+        public function getHomeXpath()
+        {
+            return null;
+        }
+    }
+}
