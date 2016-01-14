b8fd57c07366d0b2ac559511fcd3f869e627cebb
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-14 08:46:32 -0600

    Fixed the class resolution test

--- tests/AbstractTestCase/ClassResolutionTest.php
+++ tests/AbstractTestCase/ClassResolutionTest.php
@@ -25,5 +25,10 @@ namespace Magium {
         {
             return null;
         }
+
+        public function getGuaranteedPageLoadedElementDisplayedXpath()
+        {
+            return null;
+        }
     }
 }
