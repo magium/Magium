e7b06c6d6bb633e6e7915d9635025129440a09c6
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-19 07:18:10 -0600

    Added a webdriver proxy class for when you are going to want to interact with an element at a later point in time but need a WebDriverElement instance right now.

---
+++ lib/WebDriver/WebDriverElementProxy.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Magium\WebDriver;
+
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverElement;
+
+class WebDriverElementProxy implements WebDriverElement
+{
+
+    protected $webDriver;
+    protected $selector;
+    protected $by;
+    protected $element;
+
+    public function __construct(
+        WebDriver $webDriver,
+        $selector,
+        $by = WebDriver::BY_ID
+    )
+    {
+        $this->webDriver = $webDriver;
+        $this->selector = $selector;
+        $this->by = $by;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (strpos($name, '::') > 0) {
+            list($class, $name) = explode('::', $name);
+        }
+        if (!$this->element instanceof WebDriverElement) {
+            $this->element = $this->webDriver->{$this->by}($this->selector);
+        }
+        $return = call_user_func_array([$this->element, $name], $arguments);
+        return $return;
+    }
+
+    public function clear()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function click()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getAttribute($attribute_name)
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getCSSValue($css_property_name)
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getLocation()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getLocationOnScreenOnceScrolledIntoView()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getSize()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getTagName()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getText()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function isDisplayed()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function isEnabled()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function isSelected()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function sendKeys($value)
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function submit()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function getID()
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function findElement(WebDriverBy $locator)
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+    public function findElements(WebDriverBy $locator)
+    {
+        return $this->__call(__METHOD__, func_get_args());
+    }
+
+
+}
---
+++ tests/WebDriver/WebDriverProxyTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Magium\WebDriver;
+
+use Magium\AbstractTestCase;
+use Magium\WebDriver\ExpectedCondition;
+use Magium\WebDriver\WebDriverElementProxy;
+
+class WebDriverProxyTest extends AbstractTestCase
+{
+
+    public function testProxyClick()
+    {
+        $this->writePage(<<<HTML
+<div id="test">Test</div>
+<button id="clickme" onclick="alert('test')">Click</button>
+<input id="sendMeKeys">
+HTML
+);
+        $proxy = new WebDriverElementProxy(
+            $this->webdriver,
+            'test'
+        );
+
+        self::assertEquals('Test', $proxy->getText());
+
+        $proxy = new WebDriverElementProxy(
+            $this->webdriver,
+            'sendMeKeys'
+        );
+        $proxy->sendKeys('Test');
+        self::assertEquals('Test', $proxy->getAttribute('value'));
+
+        $proxy = new WebDriverElementProxy(
+            $this->webdriver,
+            'clickme'
+        );
+        $proxy->click();
+
+        $this->webdriver->wait()->until(ExpectedCondition::alertIsPresent());
+    }
+
+    protected $filename;
+
+    protected function writePage($script)
+    {
+        $script = sprintf('<html><body>%s</body></html>', $script);
+        $this->filename = tempnam(sys_get_temp_dir(), '') . '.html';
+        $fh = fopen($this->filename, 'w+');
+        fwrite($fh, $script);
+        fclose($fh);
+
+        $this->commandOpen('file://' . $this->filename);
+    }
+
+    protected function unlink()
+    {
+        unlink($this->filename);
+    }
+
+}
