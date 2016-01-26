d720079216dfa38e818d592ff1c60524d66985a8
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-25 21:10:57 -0600

    Fixed a stupid error

--- lib/Assertions/Element/NotDisplayed.php
+++ lib/Assertions/Element/NotDisplayed.php
@@ -12,9 +12,9 @@ class NotDisplayed extends AbstractSelectorAssertion
     {
 
         try {
-            $this->testCase->assertElementExists($this->selector, $$this->by);
+            $this->testCase->assertElementExists($this->selector, $this->by);
             $this->testCase->assertTrue(
-                $this->webdriver->{$this->by}($this->selector)->isDisplayed(),
+                $this->webDriver->{$this->by}($this->selector)->isDisplayed(),
                 sprintf('The element: %s, is not displayed and it should have been', $this->selector)
             );
         } catch (\Exception $e) {
