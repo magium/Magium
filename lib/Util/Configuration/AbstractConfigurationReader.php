e0d406e7077fba9075aa8f5bbc11ed1ff14aaffe
Kevin Schroeder <kschroeder@mirageworks.com>
2016-12-02 09:07:50 -0600

    Added a recursive Interface extraction for the AbstractConfigurationReader

--- lib/Util/Configuration/AbstractConfigurationReader.php
+++ lib/Util/Configuration/AbstractConfigurationReader.php
@@ -17,11 +17,34 @@ abstract class AbstractConfigurationReader
         }
 
         foreach ($reflectionClass->getInterfaceNames() as $name){
-            $classes[] = $name;
+            $interfaces[] = $name;
+            $theseInterfaces = $this->recursivelyIntrospectInterface($name);
+            $interfaces = array_merge($interfaces, $theseInterfaces);
+            $classes = array_merge($classes, $interfaces);
         }
-
+        $classes = array_unique($classes);
         $classes = array_reverse($classes);
         return $classes;
+    }
+
+    protected function recursivelyIntrospectInterface($interfaceName)
+    {
+        $allInterfaces = [];
+        $reflectionInterface = new \ReflectionClass($interfaceName);
+        $interfaces = $reflectionInterface->getInterfaces();
+        foreach ($interfaces as $interface) {
+            $interfaceList = $this->recursivelyIntrospectInterface($interface);
+            $allInterfaces = array_merge($allInterfaces, $interfaceList);
+        }
+
+        while (($parentInterface = $reflectionInterface->getParentClass()) != false) {
+            $allInterfaces[] = $parentInterface->getName();
+            $interfaceList = $this->recursivelyIntrospectInterface($parentInterface->getName());
+            $allInterfaces = array_merge($allInterfaces, $interfaceList);
+            $reflectionInterface = $parentInterface;
+        }
+
+        return $allInterfaces;
     }
 
     abstract public function configure(ConfigurableObjectInterface $object);
